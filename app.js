26380b5c535e150c9f4b8f17b42c81b28a477d40
demetcaliskan <demetcaliskann@gmail.com>
2020-12-11 00:18:06 +0300

    Connected css and images to js using a static folder

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/signup.html");
