1b9997615421ec11875ef50124e02e11b92c3aca
demetcaliskan <demetcaliskann@gmail.com>
2020-12-11 01:08:54 +0300

    connected js signup page with mailchimp site api.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,11 +12,52 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.fName);
-  console.log(req.body.lName);
-  console.log(req.body.email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/1a3d9a1a61";
+  const options = {
+    method: "POST",
+    auth: "caliskand:3566106ad5d21582e4217ee87e04d656-us7"
+  };
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+
+
+
+
+
+//API Key
+//3566106ad5d21582e4217ee87e04d656-us7
+
+//List Id
+//1a3d9a1a61
