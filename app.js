3a2c8d557c526f260f56114e9b04a06576535996
demetcaliskan <demetcaliskann@gmail.com>
2020-12-11 00:28:35 +0300

    Added body-parser to html and js

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get("/", function(req, res){
   res.sendFile(__dirname+"/signup.html");
 });
 
+app.post("/", function(req, res) {
+  console.log(req.body.fName);
+  console.log(req.body.lName);
+  console.log(req.body.email);
+});
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
