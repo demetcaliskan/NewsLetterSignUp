2e6ada8fdc47f711c1ce1aec7388a6bdc98cd4ca
demetcaliskan <demetcaliskann@gmail.com>
2020-12-11 00:14:43 +0300

    Added bootstrap html css designs and connected the html to js.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/signup.html");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
